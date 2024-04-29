b73de778f5a9ba37fdb4d37315089a69e3fc3224
Parisa-mohammadi <prs.khanmohammadi@gmail.com>
2024-04-29 16:13:16 +0330

    template folder
    template created, view prepared, website added to setting installed apps and finally in setting, templates directory changed to BASE_DIR, 'templates'

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,8 @@
 from  django.urls import path
-from website.views import home, contact_view, about_view
+from website.views import index_view, contact_view, about_view
 
 urlpatterns = [
-    path('', home),
+    path('', index_view),
     path('contact', contact_view),
     path('about', about_view)
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
+
+def index_view(request):
+    return render(request, 'website/index.html')
+
 
 def contact_view(request):
-    return HttpResponse("<h1>Contact Us</h1>")
+    return render(request,'website/contact.html')
+
 
 def about_view(request):
-    return HttpResponse("<h1>About Us</h1>")
+    return render(request,'website/about.html')
