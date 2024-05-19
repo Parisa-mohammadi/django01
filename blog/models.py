c3b12906d4c4f60e34f4c2037846f05f2588262e
Parisa-mohammadi <prs.khanmohammadi@gmail.com>
2024-05-19 15:08:41 +0330

    template tags
    used template tags for title and content in blog home
    edited  url.py and model.py and view.py for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    # author
+    title = models.CharField(max_length=250)
+    content = models.TextField(max_length=1000)
+    # image
+    # tag
+    # category = models.CharField()
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=True)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+    def __str__(self):
+        return "{}-{}".format(self.title, self.id)
